8bf03fabe07ad72f9bb360f5bb18a8284e7f0e55
Dmitry <ds.vologdin@yandex.ru>
2020-04-13 17:17:10 +0300

    mock data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/ds-vologdin/ticket-worker/storage/memory"
@@ -9,4 +10,11 @@ import (
 func main() {
 	log.Println("start server")
 	storage := memory.NewMock()
+	for ticketID := range storage.Tickets {
+		ticket := storage.Tickets[ticketID]
+		fmt.Println(ticket)
+		order := storage.OrdersSteps[ticket.Type]
+		fmt.Println("steps order:")
+		fmt.Println(order)
+	}
 }
--- storage/memory/memory_storage.go
+++ storage/memory/memory_storage.go
@@ -1,9 +1,27 @@
 package memory
 
 import (
-	"github.com/ds-vologdin/ticket-worker/storage"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
 	"github.com/ds-vologdin/ticket-worker/ticket"
-	"github.com/ds-vologdin/ticket-worker/ticket/storage"
+
+	"github.com/google/uuid"
+)
+
+const (
+	// Types of tickets
+	TicketType1 = 1
+	TicketType2 = 2
+	TicketType3 = 3
+
+	// Types of steps
+	StepType1 = 1
+	StepType2 = 2
+	StepType3 = 3
+	StepType4 = 4
 )
 
 type KeyTicketStep struct {
@@ -15,12 +33,16 @@ type TicketStorageMemory struct {
 	Tickets     map[ticket.TicketID]ticket.Ticket
 	TicketSteps map[ticket.TicketID]ticket.TicketStep
 	OrdersSteps map[ticket.TicketType][]ticket.Step
+	TicketTypes []ticket.TicketType
+	Steps       []ticket.Step
 }
 
 func (s *TicketStorageMemory) Init() error {
 	s.Tickets = make(map[ticket.TicketID]ticket.Ticket)
 	s.TicketSteps = make(map[ticket.TicketID]ticket.TicketStep)
 	s.OrdersSteps = make(map[ticket.TicketType][]ticket.Step)
+	s.TicketTypes = make([]ticket.TicketType, 0)
+	s.Steps = make([]ticket.Step, 0)
 	return nil
 }
 
@@ -36,26 +58,67 @@ func (s *TicketStorageMemory) Init() error {
 // 	return nil, nil
 // }
 
-func NewMock() *storage.TicketStorage {
-	const (
-		// Types of tickets
-		TicketType1 = 1
-		TicketType2 = 2
-		TicketType3 = 3
+// MOCK DATA
 
-		// Statuses of tickets
-		Pending   = 1
-		Processed = 2
-		Failed    = 3
-		Success   = 4
+func NewMock() *TicketStorageMemory {
 
-		// Types of steps
-		StepType1 = 1
-		StepType2 = 2
-		StepType3 = 3
-		StepType4 = 4
-	)
-	var storage TicketStorageMemory
-	storage.Init()
-	return &storage
+	var storage = &TicketStorageMemory{}
+	err := storage.Init()
+	if err != nil {
+		log.Printf("Init TicketStorageMemory error: %v", err)
+		return nil
+	}
+	storage.initMockTicketType()
+	storage.initMockSteps()
+	storage.initMockTickets(20)
+	storage.initMockOrdersSteps()
+	return storage
+}
+
+func (s *TicketStorageMemory) initMockOrdersSteps() {
+	var order = []ticket.Step{s.Steps[0], s.Steps[1], s.Steps[2], s.Steps[3]}
+	s.OrdersSteps[TicketType1] = order
+	order = []ticket.Step{s.Steps[0], s.Steps[1], s.Steps[3]}
+	s.OrdersSteps[TicketType2] = order
+	order = []ticket.Step{s.Steps[0], s.Steps[3]}
+	s.OrdersSteps[TicketType3] = order
+}
+
+func (s *TicketStorageMemory) initMockSteps() {
+	s.Steps = append(s.Steps, getNewStep(StepType1, true))
+	s.Steps = append(s.Steps, getNewStep(StepType2, true))
+	s.Steps = append(s.Steps, getNewStep(StepType3, false))
+	s.Steps = append(s.Steps, getNewStep(StepType4, true))
+}
+
+func (s *TicketStorageMemory) initMockTicketType() {
+	s.TicketTypes = append(s.TicketTypes, TicketType1)
+	s.TicketTypes = append(s.TicketTypes, TicketType2)
+	s.TicketTypes = append(s.TicketTypes, TicketType3)
+}
+
+func (s *TicketStorageMemory) initMockTickets(count int) {
+	for i := 0; i < count; i++ {
+		ticketType := s.TicketTypes[rand.Intn(len(s.TicketTypes))]
+		ticket := getNewTicket(ticketType)
+		s.Tickets[ticket.ID] = ticket
+	}
+}
+
+func getNewStep(stepType ticket.StepType, auto bool) ticket.Step {
+	return ticket.Step{Type: stepType, Auto: auto, Description: fmt.Sprintf("mock-%v", stepType)}
+}
+
+func getNewTicket(ticketType ticket.TicketType) ticket.Ticket {
+	id := getNewTicketID()
+	ticket := ticket.Ticket{ID: id, Type: ticketType, Status: ticket.Pending, Created: time.Now()}
+	return ticket
+}
+
+func getNewTicketID() ticket.TicketID {
+	id, err := uuid.NewRandom()
+	if err != nil {
+		log.Printf("getNewTicketID error: %v", err)
+	}
+	return ticket.TicketID(id)
 }
